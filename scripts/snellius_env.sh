9851edddfd961ab95aca3692db1b0e1ce3c7deba
Gabor Zavodszky <zega2@int1.local.snellius.surf.nl>
2021-11-20 15:17:03 +0100

    Adding environment script for Snellius.

---
+++ scripts/snellius_env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [[ "$0" == "$BASH_SOURCE" ]]; then
+ echo "Script is a subshell, this wont work, source it instead!"
+ exit 1
+fi
+
+# If module enviroment fails to init on older SURF module system uncomment next line
+#. /sara/sw/modules/module/init/bash
+
+module load 2021
+module load CMake/3.20.1-GCCcore-10.3.0
+module load GCC/10.3.0
+module load HDF5/1.10.7-gompi-2021a
+module load OpenMPI/4.1.1-GCC-10.3.0
+module list
