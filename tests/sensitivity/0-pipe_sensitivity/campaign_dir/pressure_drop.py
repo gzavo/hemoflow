7472879a35896b28715e4cdaf07880fd7f6efe52
sandorlevente <29997765+sandorlevente@users.noreply.github.com>
2025-10-06 16:20:14 +0200

    test: pipe sensitivity study working

--- tests/sensitivity/0-pipe_sensitivity/campaign_dir/pressure_drop.py
+++ tests/sensitivity/0-pipe_sensitivity/campaign_dir/pressure_drop.py
@@ -4,37 +4,55 @@ import glob
 import numpy as np
 import argparse
 import re
+import pandas as pd
+
 
 def mag(a: pv.pyvista_ndarray):
     "Returns the magnigude of an array of scalars/vectors."
     return np.sqrt((a * a).sum(axis=-1))
+
+
 def calculate_mean(slice):
-    slice['mean_density'] = slice['density [Pa]'] / slice['Area']
-    slice['mean_velocity'] = slice['velocity_magnitude']/ slice['Area']
-    slice['VFR']=slice['mean_velocity']*slice['Area']
+    slice["mean_density"] = slice["density [Pa]"] / slice["Area"]
+    slice["mean_velocity"] = slice["velocity_magnitude"] / slice["Area"]
+    slice["VFR"] = slice["mean_velocity"] * slice["Area"]
+
 
 def main(path):
 
-    timesteps = glob.glob(os.path.join(path,"output/**/*.vti"), recursive=True)
-    timesteps=sorted(timesteps)
-    pointdata=pv.read(timesteps[-1])
-    dx=pointdata.spacing[0]
+    timesteps = glob.glob(os.path.join(path, "output/**/*.vti"), recursive=True)
+    timesteps = sorted(timesteps)
+    pointdata = pv.read(timesteps[-1])
+    dx = pointdata.spacing[0]
     velocity_magnitude = mag(pointdata.point_data["velocity [m/s]"])
     pointdata.point_data["velocity_magnitude"] = velocity_magnitude
-    voxeldata=pointdata.point_data_to_cell_data()
-    threshold=voxeldata.threshold(value=0.0001,scalars='velocity_magnitude')
-    inlet_slice=threshold.slice(origin=[voxeldata.bounds[0]+dx,0,0], normal=[1,0,0])
-    outlet_slice=threshold.slice(origin=[voxeldata.bounds[1]-dx,0,0], normal=[1,0,0])
-    inlet=inlet_slice.integrate_data()
-    outlet=outlet_slice.integrate_data()
+    voxeldata = pointdata.point_data_to_cell_data()
+    threshold = voxeldata.threshold(value=0.0001, scalars="velocity_magnitude")
+    inlet_slice = threshold.slice(
+        origin=[voxeldata.bounds[0] + dx, 0, 0], normal=[1, 0, 0]
+    )
+    outlet_slice = threshold.slice(
+        origin=[voxeldata.bounds[1] - dx, 0, 0], normal=[1, 0, 0]
+    )
+    inlet = inlet_slice.integrate_data()
+    outlet = outlet_slice.integrate_data()
     calculate_mean(inlet)
     calculate_mean(outlet)
-    pressure_drop=(inlet['mean_density']-outlet['mean_density'])[0]
+    pressure_drop = (inlet["mean_density"] - outlet["mean_density"])[0]
 
     print(f"Pressure drop for pipe {pressure_drop}")
     print(f"Inlet VFR: {inlet['VFR'][0]:.4} m3/s")
     print(f"Outlet VFR: {outlet['VFR'][0]:.4} m3/s")
-    
+
+    dataframe = pd.DataFrame(
+        {
+            "inlet_VFR": [inlet["VFR"][0]],
+            "outlet_VFR": [outlet["VFR"][0]],
+            "pressure_drop": [pressure_drop],
+        }
+    )
+
+    dataframe.to_csv("output.csv")
 
 
 if __name__ == "__main__":
@@ -44,4 +62,3 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     main(path=os.path.abspath(args.d))
-
--- tests/sensitivity/0-pipe_sensitivity/pipe_sensitivity_campaign.py
+++ tests/sensitivity/0-pipe_sensitivity/pipe_sensitivity_campaign.py
@@ -1,109 +1,120 @@
 import easyvvuq as uq
-import chaospy as cp
-import matplotlib.pyplot as plt
 from dask.distributed import Client
 import argparse
 import os
+import time
 
-SOFTWARE_PATH='/home/nan/Documents/github/hemoflow/'
-SOFTWARE_PATH=os.path.abspath(SOFTWARE_PATH)
-#HEMOFLOW_PATH='/mnt/d/1_Github/VascuTreatCFD/hemoflowcfd/build/hemoflow'
-HEMOFLOW_PATH=os.path.join(SOFTWARE_PATH,'build','hemoFlow')
-HEMOFLOW_PATH=os.path.abspath(HEMOFLOW_PATH)
-PREPRPOCESSOR_PATH=os.path.join(SOFTWARE_PATH,'preprocessor','main.py')
-PREPRPOCESSOR_PATH=os.path.abspath(PREPRPOCESSOR_PATH)
-#For LBMpost use the fix-vvuq branch
-LBMPOST_PATH='/mnt/d/1_Github/LBMPOST/main.py'
-TEMPLATE_DIR_PATH='campaign_dir'
-TEMPLATE_DIR_PATH=os.path.abspath(TEMPLATE_DIR_PATH)
+SOFTWARE_PATH = "/home/nan/Documents/github/hemoflow/"
+SOFTWARE_PATH = os.path.abspath(SOFTWARE_PATH)
+# HEMOFLOW_PATH='/mnt/d/1_Github/VascuTreatCFD/hemoflowcfd/build/hemoflow'
+HEMOFLOW_PATH = os.path.join(SOFTWARE_PATH, "build", "hemoFlow")
+HEMOFLOW_PATH = os.path.abspath(HEMOFLOW_PATH)
+PREPRPOCESSOR_PATH = os.path.join(SOFTWARE_PATH, "preprocessor", "main.py")
+PREPRPOCESSOR_PATH = os.path.abspath(PREPRPOCESSOR_PATH)
+# For LBMpost use the fix-vvuq branch
+LBMPOST_PATH = "/mnt/d/1_Github/LBMPOST/main.py"
+TEMPLATE_DIR_PATH = "campaign_dir"
+TEMPLATE_DIR_PATH = os.path.abspath(TEMPLATE_DIR_PATH)
 
 
-def run_easy_vvuq_example(client_param):
+def run_sensitivity_study(client_param):
     """
-        Usage of EasyVVUQ for vascular simulation.
-        """
+    Usage of EasyVVUQ for simulation verification study
+    """
     work_dir = os.path.dirname(os.path.abspath(__file__))
-    campaign = uq.Campaign(
-        name="_hemoflow_sensitivity_",
-        work_dir=work_dir
-    )
+    campaign = uq.Campaign(name="temp_pipe_sensitivity_", work_dir=work_dir)
 
     params = {
-        "l": {"type": "float", "default": 1.0},
-        "q": {"type": "float", "default": 1.5},
+        "u": {"type": "float", "default": 0.45},
+        "l": {"type": "integer", "default": 0},
+        "q": {"type": "integer", "default": 0},
         "dt": {"type": "float", "default": 1e-5},
         "dx": {"type": "float", "default": 2e-4},
-        "elem": {"type": "integer", "default": 20000},
-        "save_dt": {"type": "integer", "default": 50},
+        "elem": {"type": "integer", "default": int(5e4)},
+        "save_dt": {"type": "float", "default": 10},  # default 0.016
+        "t_end": {"type": "float", "default": 0.8},  # CHANGE BACK TO 0.8!
     }
 
-    encoder = uq.encoders.GenericEncoder(template_fname='{}/campaign_dir/input.template'.format(work_dir), delimiter='$', target_filename='input.xml')
-    encoder_vox = uq.encoders.GenericEncoder(template_fname='{}/campaign_dir/vox_config.template'.format(work_dir), delimiter='$', target_filename='input/input_pipe_vox.config')
-    decoder = uq.decoders.SimpleCSV(target_filename='postProc/post_data.csv', output_columns=['ane_0_velocity_vol_avg'])
+    vary = {
+        "dt": [
+            5e-5,
+            6e-5,
+        ],
+    }
+
+    encoder = uq.encoders.GenericEncoder(
+        template_fname="{}/campaign_dir/input.template".format(work_dir),
+        delimiter="$",
+        target_filename="input.xml",
+    )
+    encoder_vox = uq.encoders.GenericEncoder(
+        template_fname="{}/campaign_dir/vox_config.template".format(work_dir),
+        delimiter="$",
+        target_filename="input/input_pipe_vox.config",
+    )
+    # decoder = uq.decoders.SimpleCSV(target_filename='postProc/post_data.csv', output_columns=['ane_0_velocity_vol_avg'])
+    decoder = uq.decoders.SimpleCSV(
+        target_filename="output.csv", output_columns=["pressure_drop"]
+    )
 
     actions = uq.actions.Actions(
         uq.actions.CreateRunDirectory(root=work_dir, flatten=True),
-        uq.actions.ExecuteLocal("cp --recursive "+TEMPLATE_DIR_PATH+"/. ./"),
+        uq.actions.ExecuteLocal("cp --recursive " + TEMPLATE_DIR_PATH + "/. ./"),
         uq.actions.Encode(encoder_vox),
-        #uq.actions.ExecuteLocal(f"conda run --live-stream -n prepost python {PREPRPOCESSOR_PATH} ./input/input_pipe_vox.config"),
+        # Voxelization
+        uq.actions.ExecuteLocal(
+            f"conda run --live-stream -n lbmpre python {PREPRPOCESSOR_PATH} ./input/input_pipe_vox.config"
+        ),
         uq.actions.Encode(encoder),
+        # Simulation
         #!RUNNING ON 6 CORES
-        #uq.actions.ExecuteLocal("mpirun -n 6 "+HEMOFLOW_PATH+" input.xml"), 
-        #conda env for running LBMpost, livestream for stdio
-        uq.actions.ExecuteLocal("conda run --live-stream -n prepost python "+LBMPOST_PATH+" ./ full"),
-        uq.actions.Decode(decoder)
+        uq.actions.ExecuteLocal("mpirun -n 6 " + HEMOFLOW_PATH + " input.xml"),
+        # conda env for running LBMpost, livestream for stdio
+        # uq.actions.ExecuteLocal("conda run --live-stream -n lbmpost python "+LBMPOST_PATH+" ./ full"),
+        uq.actions.ExecuteLocal(
+            "conda run --live-stream -n lbmpost python pressure_drop.py ./"
+        ),
+        uq.actions.Decode(decoder),
     )
 
-    campaign.add_app(
-        name="hemoflow_sensitivity_",
-        params=params,
-        actions=actions
-    )
+    campaign.add_app(name="hemoflow_sensitivity_", params=params, actions=actions)
 
-    vary = {
-        "l": cp.Normal(100, 0.1),
-        "q": cp.Normal(200, 0.1),
-    }
-
-    campaign.set_sampler(uq.sampling.SCSampler(vary=vary, polynomial_order=1))
+    campaign.set_sampler(uq.sampling.Grid_Sampler(vary))
 
     campaign.execute(pool=client_param).collate()
     print("Simulation finished.")
 
-    print("Analysis started.")
-    campaign.apply_analysis(
-        uq.analysis.SCAnalysis(
-            sampler=campaign.get_active_sampler(),
-            qoi_cols=["ane_0_velocity_vol_avg"]
-        )
-    )
+    data_frame = campaign.get_collation_result()
+    print(data_frame)
+    data_frame.to_csv("sensitivity_output.csv")
 
-    results = campaign.get_last_analysis()
-    plt.axis('off')
-
-    results.plot_sobols_treemap('ane_0_velocity_vol_avg', figsize=(10, 10), filename="result_velo.png")
-    import time
-    time.sleep(180)
+    time.sleep(5)  # why 180 sec?
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     """
     Parsing arguments to specify type of run. Possible options:
     --local - running locally.
-    --slurm - running using SLURM with default option dask-jobqueue - deploy Dask 
-              on common job queuing systems on HPC. Dask-mpi option also available 
+    --slurm - running using SLURM with default option dask-jobqueue - deploy Dask
+              on common job queuing systems on HPC. Dask-mpi option also available
               for deploying Dask from within an existing MPI environment.
     """
     parser = argparse.ArgumentParser(
         description="EasyVVUQ applied to a vertical tube deflection (using DASK).",
         epilog="",
-        formatter_class=argparse.RawTextHelpFormatter
+        formatter_class=argparse.RawTextHelpFormatter,
     )
 
-    parser.add_argument("--local", "-l", help="Run locally.", action="store_true", default=False)
-    parser.add_argument("--slurm", "-s", help="Run using SLURM. Possible options: dask-jobqueue, dask-mpi.",
-                        default="dask-jobqueue")
+    parser.add_argument(
+        "--local", "-l", help="Run locally.", action="store_true", default=False
+    )
+    parser.add_argument(
+        "--slurm",
+        "-s",
+        help="Run using SLURM. Possible options: dask-jobqueue, dask-mpi.",
+        default="dask-jobqueue",
+    )
 
     args = parser.parse_args()
 
@@ -112,33 +123,34 @@ if __name__ == '__main__':
     """
     if args.local:
         print("Running locally")
-        client = Client(processes=True,n_workers=1, threads_per_worker=1)
-        run_easy_vvuq_example(client)
+        client = Client(processes=True, n_workers=1, threads_per_worker=1)
+        run_sensitivity_study(client)
     elif args.slurm == "dask-jobqueue":
         print("Running with SLURM using dask-jobqueue.")
         from dask_jobqueue import SLURMCluster
 
         cluster = SLURMCluster(
-            shebang='#!/bin/bash -l',
+            shebang="#!/bin/bash -l",
             job_extra=[
-                '--output /net/archive/groups/plggsano/EasyVVUQ/slurm_outputs/slurm-%j.out',
-                '--error /net/archive/groups/plggsano/EasyVVUQ/slurm_outputs/slurm-%j.err'],
-            queue='plgrid-short',
-            project='plgsano2',
+                "--output /net/archive/groups/plggsano/EasyVVUQ/slurm_outputs/slurm-%j.out",
+                "--error /net/archive/groups/plggsano/EasyVVUQ/slurm_outputs/slurm-%j.err",
+            ],
+            queue="plgrid-short",
+            project="plgsano2",
             cores=24,
             processes=8,
             memory="20GB",
-            walltime='00:10:00',
-            interface='ib0',
-            scheduler_options={'interface': 'eth55'}
+            walltime="00:10:00",
+            interface="ib0",
+            scheduler_options={"interface": "eth55"},
         )
-        cluster.job_cls.submit_command = 'sbatch'
-        cluster.submit_command = 'sbatch'
+        cluster.job_cls.submit_command = "sbatch"
+        cluster.submit_command = "sbatch"
         cluster.scale(jobs=8)
         client = Client(cluster)
         print(cluster)
         print(client)
-        run_easy_vvuq_example(client)
+        run_sensitivity_study(client)
     elif args.slurm == "dask-mpi":
         print("Running with SLURM using dask-mpi.")
         from dask_mpi import initialize
@@ -149,7 +161,7 @@ if __name__ == '__main__':
         client = Client()
 
         host = client.run_on_scheduler(socket.gethostname)
-        port = client.scheduler_info()['services']['dashboard']
+        port = client.scheduler_info()["services"]["dashboard"]
         login_node_address = "login@pro.cyfronet.pl"  # Change this to the address/domain of your login node
 
         logger.info(f"ssh -N -L {port}:{host}:{port} {login_node_address}")
@@ -157,7 +169,7 @@ if __name__ == '__main__':
         from dask.distributed import performance_report
 
         with performance_report(filename="dask-report.html"):
-            run_easy_vvuq_example(client)
+            run_sensitivity_study(client)
 
         client.profile(filename="dask-profile.html")
 
